6091892d42ab236f67c613976c79e18b304494b5
slawa17705 <gold9999@list.ru>
2026-02-05 15:53:39 +0300

    Remove telegram import

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 require('dotenv').config();
 
