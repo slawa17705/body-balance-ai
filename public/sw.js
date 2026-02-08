67d8a065d8455a1d1ff5c47dcc262742f599a256
slawa17705 <60672869+slawa17705@users.noreply.github.com>
2026-02-08 14:51:20 +0300

    Delete public/sw.js

--- public/sw.js
+++
@@ -1,9 +0,0 @@
-// Простейший Service Worker
-self.addEventListener('install', event => {
-    console.log('✅ Service Worker установлен');
-});
-
-self.addEventListener('fetch', event => {
-    // Просто пропускаем все запросы
-    event.respondWith(fetch(event.request));
-});
